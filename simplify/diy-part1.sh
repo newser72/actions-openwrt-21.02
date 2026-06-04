3b303e9b4b9c43142ddc3bdc77f16954e5e49a11
newser72 <107610522+newser72@users.noreply.github.com>
2026-06-04 13:51:43 +0800

    Update diy-part1.sh

--- simplify/diy-part1.sh
+++ simplify/diy-part1.sh
@@ -21,5 +21,5 @@
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 
 # 商店
-git clone https://github.com/linkease/istore-ui package/istore-ui
-git clone https://github.com/linkease/istore package/istore
+#git clone https://github.com/linkease/istore-ui package/istore-ui
+#git clone https://github.com/linkease/istore package/istore
